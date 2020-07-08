4a324404710c8439a0b849a5ca365982ec35a475
Carlo Enrico <carlo.enrico@uol.com.br>
2020-07-07 23:13:57 -0300

    chore: added encript password test

--- JestModule/server/__tests__/integration/user.test.js
+++ JestModule/server/__tests__/integration/user.test.js
@@ -1,4 +1,6 @@
 import request from 'supertest';
+import bcrypt from 'bcrypt';
+import User from '../../src/app/models/Users';
 import app from '../../src/app';
 
 import truncate from '../util/truncate';
@@ -6,6 +8,21 @@ import truncate from '../util/truncate';
 describe('User', () => {
   beforeEach(async () => {
     await truncate();
+  });
+
+  it('verify if user added has encrypted password', async () => {
+    const user = await User.create({
+      name: 'Carlo Enrico',
+      email: 'carlo@uol.com.br',
+      password: '123456',
+    });
+
+    const comparePasswordsHash = await bcrypt.compare(
+      '123456',
+      user.password_hash
+    );
+
+    expect(comparePasswordsHash).toBe(true);
   });
 
   it('should be able to register', async () => {
--- JestModule/server/src/app/models/Users.js
+++ JestModule/server/src/app/models/Users.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcrypt';
 
 class User extends Model {
   static init(connection) {
@@ -6,6 +7,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
       {
@@ -13,6 +15,11 @@ class User extends Model {
       }
     );
 
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
     return this;
   }
 
